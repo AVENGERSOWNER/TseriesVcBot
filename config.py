faa8ef3dfcdea648466d77db0f798cb788e7ae39
JaniRPLover <fuyxycivivucyfu143@gmail.com>
2025-07-19 13:20:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Jani-Music/JaniMusic",
+    "https://github.com/JaniRPLover/Jani-private-Repo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "jani")
 GIT_TOKEN = getenv(
@@ -43,7 +43,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+AaI_GATiYwQ5NjU1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+AaI_GATiYwQ5NjU1")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots41dd4c')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsb4f439')
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
